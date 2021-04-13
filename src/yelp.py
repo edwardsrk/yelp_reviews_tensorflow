30ff114946611acd251af96ba3f801cd558e8803
Rachel <rachelke411@gmail.com>
2021-04-13 09:18:57 -0700

    src

---
+++ src/yelp.py
@@ -0,0 +1,48 @@
+def get_wordnet_pos(treebank_tag):
+    '''
+    Translate nltk POS to wordnet tags
+    '''
+    if treebank_tag.startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag.startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag.startswith('N'):
+        return wordnet.NOUN
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return wordnet.NOUN
+
+
+
+sw = stopwords.words('english')
+
+def data_cleaner(doc, stop_words=sw):
+    """A function to strip punctuation, strip stopwords, casefold, lemmatize,
+    And part pf speech tag words for clean data for modeling"""
+    
+    regex_token = RegexpTokenizer(r"([a-zA-Z]+(?:’[a-z]+)?)")
+    doc = regex_token.tokenize(doc)
+    doc = [word.lower() for word in doc]
+    doc = [word for word in doc if word not in sw]
+    #print(doc)
+    doc = pos_tag(doc)
+    doc = [(word[0], get_wordnet_pos(word[1])) for word in doc]
+    #print(doc)
+    lemmatizer = WordNetLemmatizer() 
+    doc = [lemmatizer.lemmatize(word[0], word[1]) for word in doc]
+    print(' '.join(doc))
+    return ' '.join(doc)
+
+def num_to_cat(star):
+    if star == 4 or star == 5:
+        return 'pos'
+    else:
+        return 'neg'
+
+def conf_matrix_plotter(model):
+    fig, ax = plt.subplots()
+
+    fig.suptitle(str(model))
+
+    plot_confusion_matrix(model, X_t_vec, y_t, ax=ax, cmap="plasma");
