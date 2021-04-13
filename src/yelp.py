ddff5ff8f5f0a15e27204ee5e27cbb3d29fd0311
edwardsrk <55419995+edwardsrk@users.noreply.github.com>
2021-04-13 10:22:31 -0700

    Update yelp.py

--- src/yelp.py
+++ src/yelp.py
@@ -1,10 +1,52 @@
 import nltk
+from nltk.classify.scikitlearn import SklearnClassifier
+from nltk.classify import ClassifierI
+nltk.download('punkt')
 nltk.download('tagsets')
 nltk.help.upenn_tagset()
 from nltk import pos_tag
 from nltk.stem import WordNetLemmatizer, PorterStemmer
 from nltk.corpus import stopwords, wordnet
 from nltk.tokenize import regexp_tokenize, word_tokenize, RegexpTokenizer
+
+import random
+
+import pickle #install
+
+import pandas as pd
+
+from sklearn.naive_bayes import MultinomialNB,BernoulliNB
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import StackingRegressor
+from sklearn.metrics import accuracy_score, precision_score, confusion_matrix, plot_confusion_matrix
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split, cross_val_score, cross_validate, GridSearchCV, RandomizedSearchCV
+from sklearn import metrics
+
+from statistics import mode #install
+
+import json
+
+import csv
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+import seaborn as sns
+
+from os import path
+
+from PIL import Image
+
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+
+from xgboost import XGBClassifier
 
 def get_wordnet_pos(treebank_tag):
     '''
